c9996a30510e7c7ca841668aaf6ced5b724d9fc4
Arturo Céspedes Pedrazas <cespedes.peart22@triana.salesianos.edu>
2021-10-26 10:02:42 +0200

    Se añadio modificaciones al hola mundo

--- HolaMundo/src/holamundo/holamundo.java
+++ HolaMundo/src/holamundo/holamundo.java
@@ -1,10 +1,11 @@
 package holamundo;
 
-public class Principal {
+public class holamundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo <3");
+		System.out.println("Como estan chicos");
 	}
 
 }
